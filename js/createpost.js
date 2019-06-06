ec4d4f318c149a1a4ea21f6f2c9978a1e7835a92
lesi-sampson <lesisampson@gmail.com>
2019-06-06 17:04:09 +0100

    add data to database

---
+++ js/createpost.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $('#createblogpost').click(function(event){
+        event.preventDefault();         
+        var postTitle = $("#nameofarticle").val()
+        var postSubtitle = $("#subheading").val()
+        var postBody = $("#blogpost").val()
+        var dateOfpost =$("#dateofpost").val()
+        let blogData = {postTitle,postSubtitle,dateOfpost,postBody}
+        if (posttitle && postsubtitle && postbody){
+            $.ajax({
+                method: "POST",
+                url: "http://localhost:3000/blogposts",
+                // dataType: "json",
+                data: blogdata,
+                success: function(response) {
+                    alert('database updated');
+                },
+                error: function(error){
+                    alert(error);
+                }
+              })
+              
+        }
+
+
+    })
+})  
