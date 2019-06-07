99ff0429539587f1fbd3740e14689b2f3b16d857
lesi-sampson <lesisampson@gmail.com>
2019-06-07 18:44:56 +0100

    show comment on each blogpost

--- js/createcommentsdatabase.js
+++ js/createcommentsdatabase.js
@@ -12,28 +12,42 @@ $(document).ready(function(){
                 url: "http://localhost:3000/comments",
                 // dataType: "json",
                 data:commentdata,
-                success: function(response) {
+                /*success: function(response) {
                     alert('database updated');
                 },
                 error: function(error){
                     alert(error);
-                }
+                }*/
               })
               $.ajax({
                 method: "GET",
                 url: "http://localhost:3000/comments",
                 // dataType: "json",
                 data:commentdata,
-                success: function(response) {
+                /*success: function(response) {
                     alert('data gotten');
                 },
                 error: function(error){
                     alert(error);
-                }
+                }*/
             })
         }
 
     })
-          
+    
+
+    let blogpostid =window.location.search.substring(1);
+    $.ajax({
+        method:'GET',
+        dataType:'json',
+        url: 'http://localhost:3000/comments'
+      }).done(function(data){
+             $.map(data, function(blogs,i){
+                if(blogpostid===blogs.blogpostid){
+                    $('#showcomment').append(`<p>${blogs.username} : ${blogs.comment}</p>`);
+                }
+             })
+    
+        })
   
 });
